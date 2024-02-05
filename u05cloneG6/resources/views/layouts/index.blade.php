c3cccda3c38ca899abcce67f37efb4b445441212
Denize <denize.yildirim@chasacademy.se>
2024-02-05 10:11:36 +0000

    created new blade files

---
+++ u05cloneG6/resources/views/layouts/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+@section('content')
+<div>Index of movies</div>
+
+@endsection
--- u05cloneG6/routes/web.php
+++ u05cloneG6/routes/web.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::view('/', '/layouts/index');
+Route::view('/movies', 'show');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+ 
