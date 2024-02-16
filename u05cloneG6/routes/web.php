d877fa2d9c99ca23152229743dc8697f20ccb2f7
Denjin <denjin.muratagic@chasacademy.se>
2024-02-16 14:37:45 +0000

    Connected my Controller to the Routes-folder

--- u05cloneG6/routes/web.php
+++ u05cloneG6/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WatchlistController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,5 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+Route::get('/WatchlistController', [WatchlistController::class, 'index']);
 
 require __DIR__.'/auth.php';
