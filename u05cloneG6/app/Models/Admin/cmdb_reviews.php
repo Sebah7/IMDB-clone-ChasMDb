dc42b4e3ceccd40ab976cab8e37e99e25645e6cd
jacobbirke <birkejacob@gmail.com>
2024-02-19 13:14:37 +0000

    Reviews Model

--- u05cloneG6/app/Models/Admin/cmdb_reviews.php
+++ u05cloneG6/app/Models/Admin/cmdb_reviews.php
@@ -1,11 +1,27 @@
 <?php
 
 namespace App\Models\Admin;
+namespace App\Models;
 
+use App\Models\Admin\cmdb_movies;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class cmdb_reviews extends Model
 {
     use HasFactory;
+
+    protected $table = 'cmdb_reviews';
+    protected $fillable = ['Movieid', 'Stars', 'Comment', 'Userid'];
+
+    public function userReviewRelationship()
+    {
+        return $this->belongsTo(User::class, 'Userid');
+    }
+
+    public function movie()
+    {
+        return $this->belongsTo(cmdb_movies::class, 'Movieid');
+    }
+
 }
