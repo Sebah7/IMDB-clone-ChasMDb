6edaf022c35c7d42a0a2e13e59f8f7afb28588f8
Sebah Abubeker <sebahismail21@gmail.com>
2024-02-19 09:12:23 +0000

    Declared the relation with movies model and movies_genre pivot migration.

--- u05cloneG6/app/Models/Admin/cmdb_genre.php
+++ u05cloneG6/app/Models/Admin/cmdb_genre.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class cmdb_genre extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    protected $tabel = 'cmdb_genres';
+    protected $fillable = ['name'];
+
+   // the movies function declares a method to return the relation definition between genres and movies. 
+   // This is where the pivot table is mentioned to connect the two models.
+    public function movies()
+    {
+        return $this->belongsToMany(cmdb_movies::class, 'cmdb_movies_genre_table_pivot', 'genre_id', 'movie_id');
+    }
 }
+
