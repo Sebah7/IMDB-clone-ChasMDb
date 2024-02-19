e4de6f7bc8430df40187581423fa7043d13992e5
Sebah Abubeker <sebahismail21@gmail.com>
2024-02-19 12:36:30 +0000

    Testing genre controller route.

--- u05cloneG6/routes/web.php
+++ u05cloneG6/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GenreController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,4 +29,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/genres', [GenreController::class, 'index']) ->name('genres.index');
+Route::get('/genres/{id}', [GenreController::class, 'show']) ->name('genres.show');
 require __DIR__.'/auth.php';
