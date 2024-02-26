2959ca6f5f11b969ec14888f9366bbf35de810db
EliasProjects <149054821+EliasProjects@users.noreply.github.com>
2024-02-26 12:28:45 +0100

    Update dashboard.blade.php
    Elias #93

--- u05cloneG6/resources/views/dashboard.blade.php
+++ u05cloneG6/resources/views/dashboard.blade.php
@@ -55,36 +55,7 @@
     </header>
 
     <!-- Main content -->
-    <!-- <div class="py-20">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 rounded-lg shadow overflow-hidden">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <x-app-layout>
-                        <x-slot name="header">
-                            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                                {{ __('Dashboard') }}
-                            </h2>
-                        </x-slot>
-
-                        <div class="py-12">
-                            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                                        You're logged in as
-                                        @if (Auth::user()->role)
-                                            user
-                                        @else
-                                            admin
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </x-app-layout>
-                </div>
-            </div>
-        </div>
-    </div> -->
+    
 
 </body>
 
