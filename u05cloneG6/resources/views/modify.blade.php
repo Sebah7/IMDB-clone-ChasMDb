ef83ed380ca23c2909a89c20fb072c882b27f851
Denize <denize.yildirim@chasacademy.se>
2024-02-26 13:35:22 +0000

    added a form to movies.store in modify

--- u05cloneG6/resources/views/modify.blade.php
+++ u05cloneG6/resources/views/modify.blade.php
@@ -44,6 +44,26 @@
             <button type="submit">
                 Add Actor
             </button>
+        </form><br><br>
+
+        <form action="{{ route('movies.store') }}" method="post">
+            @csrf
+            <label for="title">Title:</label>
+            <input type="text" id="title" name="title" required maxlength="255"><br>
+
+            <label for="actor">Actor:</label>
+            <input type="text" id="actor" name="actor" required maxlength="255"><br>
+
+            <label for="director">Director:</label>
+            <input type="text" id="director" name="director" required maxlength="255"><br>
+
+            <label for="genre">Genre:</label>
+            <input type="text" id="genre" name="genre" required maxlength="255">
+
+            <!-- Add more input fields as needed --><br>
+
+
+            <button type="submit">Create Movie</button>
         </form>
 </body>
 
