a6938e7e279a499b7eedebb6ad2425a608bf6f31
EliasProjects <149054821+EliasProjects@users.noreply.github.com>
2024-02-27 11:33:49 +0100

    Update update-profile-information-form.blade.php
    Elias working on #93

--- u05cloneG6/resources/views/profile/partials/update-profile-information-form.blade.php
+++ u05cloneG6/resources/views/profile/partials/update-profile-information-form.blade.php
@@ -51,16 +51,6 @@
 
     <!-- Profile Information Section -->
     <section>
-        <header>
-            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                Profile Information
-            </h2>
-
-            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                Update your account's profile information and email address.
-            </p>
-        </header>
-
         <form id="send-verification" method="post" action="#">
             @csrf
         </form>
