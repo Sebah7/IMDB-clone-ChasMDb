ba4a7c32225708f25a0efc072bac99c36db59b18
EliasProjects <149054821+EliasProjects@users.noreply.github.com>
2024-02-28 15:39:07 +0100

    Update movies.blade.php

--- u05cloneG6/resources/views/movies.blade.php
+++ u05cloneG6/resources/views/movies.blade.php
@@ -1,7 +1,3 @@
-<!-- This comment is to Elias/Frontend
-This is the the blade where we want all our movies to be visble. 
-Below is the whole movie table and the data in relation from other models-->
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,40 +6,20 @@ Below is the whole movie table and the data in relation from other models-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movies</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <style>
+        .moviesection {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around; /* Optional: Adjust alignment as needed */
+            padding: 20px; /* Optional: Add padding */
+        }
+    </style>
 </head>
 
 <body class="bg-gray-100 dark:bg-gray-900">
     <!-- Navigation bar -->
     <header class="bg-red" style="position: fixed; width: 100%; margin-top: 0px; background-color: rgb(246, 236, 255); box-shadow: 2px 2px 20px 2px rgba(137, 43, 226, 0.098);">
-        <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global" style="padding: 20px 20px;">
-            <div class="flex lg:flex-1" style="margin-top: -11px;">
-                <h1>
-                    <a href="#" class="-m-1.5 p-1.5" style="font-size: 30px; font-weight: 900;">
-                        CMDb
-                    </a>
-                </h1>
-            </div>
-            <div class="flex lg:hidden">
-                <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
-                </button>
-            </div>
-            <div class="hidden lg:flex lg:gap-x-12">
-                <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Movies</a>
-                <a href="#" class="text-sm font-semibold leading-6 text-gray-900">My Reviews</a>
-                <a href="#" class="text-sm font-semibold leading-6 text-gray-900">My Watchlist</a>
-            </div>
-            <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-                <span id="user-info" class="text-sm font-semibold leading-6 text-gray-900" style="margin-top: 7px;"></span>
-                <form action="/logout" method="post">
-                    @csrf
-                    <button type="submit" class="text-white text-sm font-semibold leading-6 ml-4 bg-gradient-to-r from-purple-600 to-indigo-600 px-4 py-2 rounded-md">Logout</button>
-                </form>
-            </div>
-        </nav>
+        <!-- Your navigation bar code here -->
     </header>
 
     <br /><br /><br /><br />
@@ -51,51 +27,53 @@ Below is the whole movie table and the data in relation from other models-->
     <!-- Main content -->
 
     <div class="moviesection">
-    <div class="py-20" style="display:flex;>
-        <div>
+        <div class="py-20" style="display:flex;">
             <div>
                 <div>
-                    <!-- Movies -->
-                    <ul>
-                        <h2>{{ __('All Movies') }}</h2>
-                        @foreach ($movies as $movie)
-                        
-                        <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-white-800 dark:border-white-700" style="border-style: solid; padding: 0; width: 200px;">
-                            <a href="#">
-                                <img class="" src="{{ $movie->poster }}" alt="movie image" />
-                            </a>
-                            <div class="px-5 pb-5">
+                    <div>
+                        <!-- Movies -->
+                        <ul>
+                            <h2>{{ __('All Movies') }}</h2>
+                            @foreach ($movies as $movie)
+                            
+                            <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-white-800 dark:border-white-700" style="border-style: solid; padding: 0; width: 200px;">
                                 <a href="#">
-                                    <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ $movie->title }}</h5>
+                                    <img class="" src="{{ $movie->poster }}" alt="movie image" />
                                 </a>
-                                <div class="flex items-center mt-2.5 mb-5">
-                                    <div class="flex items-center space-x-1 rtl:space-x-reverse">
-                                        @for ($i = 0; $i < $movie->ratings; $i++)
-                                        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                                        </svg>
-                                        @endfor
-                                        @for ($i = $movie->ratings; $i < 5; $i++)
-                                        <svg class="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                                        </svg>
-                                        @endfor
+                                <div class="px-5 pb-5">
+                                    <a href="#">
+                                        <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ $movie->title }}</h5>
+                                    </a>
+                                    <div class="flex items-center mt-2.5 mb-5">
+                                        <div class="flex items-center space-x-1 rtl:space-x-reverse">
+                                            @for ($i = 0; $i < $movie->ratings; $i++)
+                                            <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                                            </svg>
+                                            @endfor
+                                            @for ($i = $movie->ratings; $i < 5; $i++)
+                                            <svg class="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                                            </svg>
+                                            @endfor
+                                        </div>
+                                        <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">{{ $movie->ratings }}</span>
                                     </div>
-                                    <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">{{ $movie->ratings }}</span>
-                                </div>
-                                <div class="flex items-center justify-between">
-                                    <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Preview</a>
-                                    <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">+</a>
+                                    <div class="flex items-center justify-between">
+                                        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Preview</a>
+                                        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">+</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        @endforeach
-                    </ul>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+    <!-- Footer and other content if any -->
 
 </body>
 
