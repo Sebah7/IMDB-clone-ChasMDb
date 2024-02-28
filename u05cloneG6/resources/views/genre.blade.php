f032218cd67a49ea9bb1b8a0fbddd2a680804d43
jacobbirke <birkejacob@gmail.com>
2024-02-28 10:00:03 +0000

    Changed title name and fixed welcome.blade routing

--- u05cloneG6/resources/views/genre.blade.php
+++ u05cloneG6/resources/views/genre.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Genres</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <title>Genres</title>
+
 </head>
 
 <body class="bg-gray-100 dark:bg-gray-900">
--- u05cloneG6/resources/views/userdashboard.blade.php
+++ u05cloneG6/resources/views/userdashboard.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <title>Userdashboard</title>
 </head>
 
 <body class="bg-gray-100 dark:bg-gray-900">
