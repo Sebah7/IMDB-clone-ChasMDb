3f8ca543badb0b3ce70969854b335095b7acc942
EliasProjects <149054821+EliasProjects@users.noreply.github.com>
2024-02-28 12:51:21 +0100

    Update movies.blade.php
    Elias #93

--- u05cloneG6/resources/views/movies.blade.php
+++ u05cloneG6/resources/views/movies.blade.php
@@ -49,55 +49,52 @@ Below is the whole movie table and the data in relation from other models-->
     <br /><br /><br /><br />
 
     <!-- Main content -->
-    <div class="py-20">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 rounded-lg shadow overflow-hidden">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <!-- Movies -->
-                    <ul style="box-shadow: 2px 2px 20px 2px rgb(227, 227, 227); padding: 10px 20px; margin-top:30px;">
-                        <h2>{{ __('All Movies') }}</h2>
-                        @foreach ($movies as $movie)
-                        
-                        <p style="font-weight: 700;">Movie name: {{ $movie->title }}</p>
-                        <!-- Each foreach below fetches data from the related models of the movie -->
-                        <!-- <p>Actors:
-                        @foreach ($movie->actors as $actor)
-                        {{ $actor->name }},
-                        @endforeach
-                        </p>
-
-                        <p>Genres:
-                        @foreach ($movie->genres as $genre)
-                        {{ $genre->name }},
-                        @endforeach
-                        </p>
-
-                        <p>Director:
-                        @foreach ($movie->directors as $director)
-                        {{ $director->name }},
-                        @endforeach
-                        </p> -->
-
-                        <p>Ratings: {{ $movie->ratings }}</p>
-
-                        <!-- the !! is used to display the html tags. And in this cas it is used to display the trailer -->
-                        <!-- <p>Trailer: {!! $movie->trailer !!}</p> -->
-
-                        <!-- the img tag is to display the poster -->
-                        <p><!--Poster:--> <img src="{{ $movie->poster }}" alt="Poster"></p>
-                        <br><br>
-
-                        <!-- <p>Runtime: {{ $movie->runtime }}</p>
-
-                        <p>Language: {{ $movie->language }}</p>
-
-                        <p>Description: {{ $movie->description }}</p> -->
-                        @endforeach
-                    </ul>
-                </div>
+    <!-- Main content -->
+<div class="py-20">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="bg-white dark:bg-gray-800 rounded-lg shadow overflow-hidden">
+            <div class="p-6 text-gray-900 dark:text-gray-100">
+                <!-- Movies -->
+                <ul style="box-shadow: 2px 2px 20px 2px rgb(227, 227, 227); padding: 10px 20px; margin-top:30px;">
+                    <h2>{{ __('All Movies') }}</h2>
+                    @foreach ($movies as $movie)
+                    
+                    <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-white-800 dark:border-white-700" style="border-style: solid; padding: 0; width: 200px;">
+                        <a href="#">
+                            <img class="" src="{{ $movie->poster }}" alt="movie image" />
+                        </a>
+                        <div class="px-5 pb-5">
+                            <a href="#">
+                                <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ $movie->title }}</h5>
+                            </a>
+                            <div class="flex items-center mt-2.5 mb-5">
+                                <div class="flex items-center space-x-1 rtl:space-x-reverse">
+                                    @for ($i = 0; $i < $movie->ratings; $i++)
+                                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                                    </svg>
+                                    @endfor
+                                    @for ($i = $movie->ratings; $i < 5; $i++)
+                                    <svg class="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                                    </svg>
+                                    @endfor
+                                </div>
+                                <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">{{ $movie->ratings }}</span>
+                            </div>
+                            <div class="flex items-center justify-between">
+                                <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Preview</a>
+                                <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">+</a>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                </ul>
             </div>
         </div>
     </div>
+</div>
+
 </body>
 
 </html>
