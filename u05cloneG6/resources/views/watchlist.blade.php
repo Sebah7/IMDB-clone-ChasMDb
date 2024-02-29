0273b9766c0cd12e37473ba26d66b5cad1dced26
Sebah Abubeker <125797927+Sebah7@users.noreply.github.com>
2024-02-29 09:42:37 +0100

    links in userdash and watchlist

--- u05cloneG6/resources/views/watchlist.blade.php
+++ u05cloneG6/resources/views/watchlist.blade.php
@@ -22,8 +22,8 @@
                 </button>
             </div>
             <div class="hidden lg:flex lg:gap-x-12">
-                <a href="/movies" class="text-sm font-semibold leading-6 text-gray-900">Movies</a>
-                <a href="/genres" class="text-sm font-semibold leading-6 text-gray-900">Genres</a>
+            <a href="/movies" class="text-sm font-semibold leading-6 text-gray-900">Movies</a>
+                <a href="/userdashboard" class="text-sm font-semibold leading-6 text-gray-900">My Reviews</a>
                 <a href="/watchlist" class="text-sm font-semibold leading-6 text-gray-900">Watchlist</a>
             </div>
             <div class="hidden lg:flex lg:flex-1 lg:justify-end">
