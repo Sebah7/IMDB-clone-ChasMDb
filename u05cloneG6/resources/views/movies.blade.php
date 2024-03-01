c33a2bae0ef25fe7ec5b0ae252f1f21dfac2d94c
EliasProjects <149054821+EliasProjects@users.noreply.github.com>
2024-03-01 15:12:54 +0100

    Update movies.blade.php

--- u05cloneG6/resources/views/movies.blade.php
+++ u05cloneG6/resources/views/movies.blade.php
@@ -120,7 +120,7 @@
                     @csrf
                     @method('DELETE')
                     <button type="submit"
-                        class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Delete</button>
+                        class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">x</button>
                 </form>
                 @endif
                 @if (session('movie_delete_success'))
