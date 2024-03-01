7d0a7279cc70cb9df7ad347003f7ac441289158b
EliasProjects <149054821+EliasProjects@users.noreply.github.com>
2024-03-01 16:32:05 +0100

    Update welcome.blade.php

--- u05cloneG6/resources/views/welcome.blade.php
+++ u05cloneG6/resources/views/welcome.blade.php
@@ -38,6 +38,7 @@
         <div class="hidden lg:flex lg:gap-x-12">
             <a href="/movies" class="text-sm font-semibold leading-6 text-gray-900">Movies</a>
             <a href="/genres" class="text-sm font-semibold leading-6 text-gray-900">Genres</a>
+            <a href="/cast" class="text-sm font-semibold leading-6 text-gray-900">Cast</a>
             <a href="/watchlist" class="text-sm font-semibold leading-6 text-gray-900">Watchlist</a>
         </div>
         <div class="hidden lg:flex lg:flex-1 lg:justify-end">
