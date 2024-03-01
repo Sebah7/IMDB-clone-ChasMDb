24a211eb4cc1a82faa2c888681da0b0dcfa89731
EliasProjects <149054821+EliasProjects@users.noreply.github.com>
2024-03-01 18:05:43 +0100

    Update userdashboard.blade.php

--- u05cloneG6/resources/views/userdashboard.blade.php
+++ u05cloneG6/resources/views/userdashboard.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
