e8d89061871d03c4af4cce1ef9794c116bada2bf
EliasProjects <149054821+EliasProjects@users.noreply.github.com>
2024-03-01 12:22:32 +0100

    Update edit.blade.php

--- u05cloneG6/resources/views/profile/edit.blade.php
+++ u05cloneG6/resources/views/profile/edit.blade.php
@@ -52,17 +52,31 @@
                     </a>
                 </h1>
             </div>
+            <div class="flex lg:hidden">
+                <button type="button"
+                    class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                        aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                </button>
+            </div>
             <div class="hidden lg:flex lg:gap-x-12">
                 <a href="/movies" class="text-sm font-semibold leading-6 text-gray-900">Movies</a>
                 <a href="/genres" class="text-sm font-semibold leading-6 text-gray-900">Genres</a>
                 <a href="/watchlist" class="text-sm font-semibold leading-6 text-gray-900">Watchlist</a>
             </div>
             <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-                <span id="user-info" class="text-sm font-semibold leading-6 text-gray-900 mr-4" style="margin-top:7px;">{{ Auth::user()->name }}</span>
+                
+                <a href="/home" style="margin-top:8px; padding:10px 14px;"><button type="submit"
+                    class="text-white text-sm font-semibold leading-6 ml-4 bg-gradient-to-r from-purple-600 to-indigo-600 px-4 py-2 rounded-md">{{ Auth::user()->name }}</button>
+                </a>
                 <form action="/logout" method="post">
                     @csrf
                     <button type="submit"
-                        class="text-white text-sm font-semibold leading-6 bg-gradient-to-r from-purple-600 to-indigo-600 px-4 py-2 rounded-md">Logout</button>
+                        class="text-white text-sm font-semibold leading-6 ml-4 bg-gradient-to-r from-purple-600 to-indigo-600 px-4 py-2 rounded-md">Logout</button>
                 </form>
             </div>
         </nav>
