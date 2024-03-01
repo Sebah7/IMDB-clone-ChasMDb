2c6c8c169345e887062a97144c590199c148ad01
EliasProjects <149054821+EliasProjects@users.noreply.github.com>
2024-03-01 17:31:29 +0100

    Update onemovie.blade.php

--- u05cloneG6/resources/views/onemovie.blade.php
+++ u05cloneG6/resources/views/onemovie.blade.php
@@ -1,4 +1,4 @@
-
+<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 <header class="bg-red" style="position: fixed; width: 100%; margin-top: 0px; background-color: rgb(246, 236, 255); box-shadow: 2px 2px 20px 2px rgba(137, 43, 226, 0.098);">
     <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global" style="padding: 20px 20px;">
         <div class="flex lg:flex-1" style="margin-top: -11px;">
@@ -20,6 +20,7 @@
             <a href="/movies" class="text-sm font-semibold leading-6 text-gray-900">Movies</a>
             <a href="/genres" class="text-sm font-semibold leading-6 text-gray-900">Genres</a>
             <a href="/cast" class="text-sm font-semibold leading-6 text-gray-900">Cast</a>
+            <a href="/userdashboard" class="text-sm font-semibold leading-6 text-gray-900">Reviews</a>
             <a href="/watchlist" class="text-sm font-semibold leading-6 text-gray-900">Watchlist</a>
         </div>
         <div class="hidden lg:flex lg:flex-1 lg:justify-end">
@@ -39,6 +40,7 @@
         </div>
     </nav>
 </header>
+<br /><br /><br />
 
 
 <br /><br /><br />
