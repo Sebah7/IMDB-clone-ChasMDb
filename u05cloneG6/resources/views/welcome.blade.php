e14d66e1e9beaed210b91bbd51c2f789af29501a
EliasProjects <149054821+EliasProjects@users.noreply.github.com>
2024-03-01 16:06:44 +0100

    Update welcome.blade.php

--- u05cloneG6/resources/views/welcome.blade.php
+++ u05cloneG6/resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <title>CMDb-G6</title>
