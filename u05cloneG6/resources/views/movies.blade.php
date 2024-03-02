be79467ff9fccf06e197682896d3c231259a1692
EliasProjects <149054821+EliasProjects@users.noreply.github.com>
2024-03-02 13:50:33 +0100

    Update movies.blade.php

--- u05cloneG6/resources/views/movies.blade.php
+++ u05cloneG6/resources/views/movies.blade.php
@@ -86,92 +86,64 @@
 
 <div class="moviesection flex flex-wrap justify-center">
     @foreach ($movies as $movie)
-    <a href="/onemovie" ><div class="movie-card m-4" style="border-style: solid; padding: 0; width: 200px; height:300px;">
+    <div class="movie-card m-4" style="border-style: solid; padding: 0; width: 200px; height:300px;">
         <a href="#">
             <img class="movie-poster" src="{{ $movie->poster }}" alt="movie image" />
         </a>
-        
-                                    <style>
-                                        .movie-poster {
-                                            width: 100%;
-                                            height: 50%;
-                                            object-fit: cover;
-                                        }
 
-                                    </style>
         <div class="movie-card-content">
-
-            <a href="#">
-                <img class="movie-poster" src="{{ $movie->poster }}" alt="movie image" />
+            <a href="{{ route('onemovie.showPreview', $movie->title) }}">
+                <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ $movie->title }}</h5>
             </a>
-
-            <style>
-                .movie-poster {
-                    width: 100%;
-                    height: 50%;
-                    object-fit: cover;
-                }
-            </style>
-            <div class="movie-card-content">
-                <a href="{{ route('onemovie.showPreview', $movie->title) }}">
-                    <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ $movie->title }}</h5>
-                </a>
-                <div class="flex items-center mt-2.5 mb-5">
-                    <div class="flex items-center space-x-1 rtl:space-x-reverse">
-                        @for ($i = 0; $i < $movie->ratings; $i++)
-                            <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                            </svg>
-                            @endfor
-                            @for ($i = $movie->ratings; $i < 5; $i++) <svg class="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                </svg>
-                                @endfor
-                    </div>
-                    <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">{{ $movie->ratings }}</span>
+            <div class="flex items-center mt-2.5 mb-5">
+                <div class="flex items-center space-x-1 rtl:space-x-reverse">
+                    @for ($i = 0; $i < $movie->ratings; $i++)
+                        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                    @endfor
+                    @for ($i = $movie->ratings; $i < 5; $i++)
+                        <svg class="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                    @endfor
                 </div>
-                <div class="flex items-center justify-between">
-                    <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Preview</a>
-                    <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">+</a>
-                    @if (auth()->user() && auth()->user()->role == '0')
+                <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">{{ $movie->ratings }}</span>
+            </div>
+
+            <div class="flex items-center justify-between">
+                <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Preview</a>
+                <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">+</a>
+                @if (auth()->user() && auth()->user()->role == '0')
                     <form action="{{ route('movies.destroy', $movie->id) }}" method="POST">
                         @csrf
                         @method('DELETE')
                         <button type="submit" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Delete</button>
                     </form>
-                    @endif
-                    @if (session('movie_delete_success'))
-                    <div class="alert alert-success">
-                        {{ session('movie_delete_success') }}
-                    </div>
-                    @endif
-                </div>
-
-                <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">{{ $movie->ratings }}</span>
+                @endif
             </div>
-            <div class="flex items-center justify-between">
-                <a href="/watchlist" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add To Watchlist</a>
-                @if (auth()->user() && auth()->user()->role == '0')
+            @if (session('movie_delete_success'))
+                <div class="alert alert-success">
+                    {{ session('movie_delete_success') }}
+                </div>
+            @endif
+        </div>
+        
+        <div class="flex items-center justify-between">
+            <a href="/watchlist" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add To Watchlist</a>
+            @if (auth()->user() && auth()->user()->role == '0')
                 <form action="{{ route('movies.destroy', $movie->id) }}" method="POST">
                     @csrf
                     @method('DELETE')
-                    <button style="padding: 10px; margin-left: 4px;" type="submit"
-                        class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">x</button>
+                    <button style="padding: 10px; margin-left: 4px;" type="submit" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">x</button>
                 </form>
-                @endif
-                @if (session('movie_delete_success'))
-        <div class="alert alert-success">
-            {{ session('movie_delete_success') }}
+            @endif
+            @if (session('movie_delete_success'))
+                <div class="alert alert-success">
+                    {{ session('movie_delete_success') }}
+                </div>
+            @endif
         </div>
-        @endif
-            </div>
-        </div>
-    </div></a>
-    
+    </div>
     @endforeach
 </div>
-
-
-</body>
-
-</html>
