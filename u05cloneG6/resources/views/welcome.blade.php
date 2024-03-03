5e1ff176b5b8c211d9e43a60a859f7b5cb416601
EliasProjects <149054821+EliasProjects@users.noreply.github.com>
2024-03-03 13:38:25 +0100

    Update welcome.blade.php

--- u05cloneG6/resources/views/welcome.blade.php
+++ u05cloneG6/resources/views/welcome.blade.php
@@ -30,10 +30,32 @@
             <div class="flex lg:hidden">
                 <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
                     <span class="sr-only">Open main menu</span>
-                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                    <svg class="navbaricone h-6 w-6" onclick="toggleNavbarLinks()" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
                 </button>
+            </div>
+            <style>
+                .navbarlinks___ {
+                    visibility: hidden;
+                }
+            </style>
+            <script>
+                function toggleNavbarLinks() {
+                    var navbarLinks = document.getElementById("navbarLinks");
+                    if (navbarLinks.style.visibility === "visible") {
+                        navbarLinks.style.visibility = "hidden";
+                    } else {
+                        navbarLinks.style.visibility = "visible";
+                    }
+                }
+            </script>
+            <div class="navbarlinks___" id="navbarLinks">
+                <a href="/movies" class="text-sm font-semibold leading-6 text-gray-900">Movies</a>
+                <a href="/genres" class="text-sm font-semibold leading-6 text-gray-900">Genres</a>
+                <a href="/cast" class="text-sm font-semibold leading-6 text-gray-900">Cast Library</a>
+                <a href="/userdashboard" class="text-sm font-semibold leading-6 text-gray-900">Reviews</a>
+                <a href="/watchlist" class="text-sm font-semibold leading-6 text-gray-900">Watchlist</a>
             </div>
             <div class="hidden lg:flex lg:gap-x-12">
                 <a href="/movies" class="text-sm font-semibold leading-6 text-gray-900">Movies</a>
