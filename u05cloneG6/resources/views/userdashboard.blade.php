74fcf05f18fb448772e0e3dbc758f46fbb59c136
EliasProjects <149054821+EliasProjects@users.noreply.github.com>
2024-03-03 14:28:27 +0100

    Update userdashboard.blade.php

--- u05cloneG6/resources/views/userdashboard.blade.php
+++ u05cloneG6/resources/views/userdashboard.blade.php
@@ -10,6 +10,7 @@
 
 <body class="bg-gray-100 dark:bg-gray-900">
     <!-- Navigation bar -->
+
     <header class="bg-red" style="position: fixed; width: 100%; margin-top: 0px; background-color: rgb(246, 236, 255); box-shadow: 2px 2px 20px 2px rgba(137, 43, 226, 0.098);">
         <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global" style="padding: 20px 20px;">
             <div class="flex lg:flex-1" style="margin-top: -11px;">
@@ -22,10 +23,58 @@
             <div class="flex lg:hidden">
                 <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
                     <span class="sr-only">Open main menu</span>
-                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                    <svg class="navbaricone h-6 w-6" onclick="toggleNavbarLinks()" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
                 </button>
+            </div>
+            <style>
+                .navbarlinks___ {
+                    visibility: hidden;
+                    background-color: rgb(246, 236, 255);
+                    width:100%;
+                    padding: 5px 15px;
+                    position: fixed;
+                    top:75px;
+                    left: 0px;
+                }
+                .navbarlinks___ a{
+                    box-shadow: 2px 2px 20px 2px rgba(137, 43, 226, 0.098);
+                    width:100%;
+                    padding: 10px;
+                    border-radius:10px;
+                }
+            </style>
+            <script>
+                function toggleNavbarLinks() {
+                    var navbarLinks = document.getElementById("navbarLinks");
+                    if (navbarLinks.style.visibility === "visible") {
+                        navbarLinks.style.visibility = "hidden";
+                    } else {
+                        navbarLinks.style.visibility = "visible";
+                    }
+                }
+            </script>
+            <div class="navbarlinks___" id="navbarLinks">
+                <a href="/movies" class="text-sm font-semibold leading-6 text-gray-900">Movies</a><br /><br />
+                <a href="/genres" class="text-sm font-semibold leading-6 text-gray-900">Genres</a><br /><br />
+                <a href="/cast" class="text-sm font-semibold leading-6 text-gray-900">Cast Library</a><br /><br />
+                <a href="/userdashboard" class="text-sm font-semibold leading-6 text-gray-900">Reviews</a><br /><br />
+                <a href="/watchlist" class="text-sm font-semibold leading-6 text-gray-900">Watchlist</a><br /><br /><br />
+
+                @auth
+
+                <a href="/home" style="margin-top:8px;"><span id="user-info" class="text-sm font-semibold leading-6 text-white bg-gradient-to-r from-purple-600 to-indigo-600 px-4 py-2 rounded-md mr-4" style="margin-top:17px; padding:10px 14px;">
+                        {{ Auth::user()->name }}</a>
+                </span>
+                <form action="/logout" method="post">
+                    @csrf
+                    <button type="submit" class="text-white text-sm font-semibold leading-6 ml-4 bg-gradient-to-r from-purple-600 to-indigo-600 px-4 py-2 rounded-md">Logout</button>
+                </form>
+                @else
+                <a href="/register" class="text-sm font-semibold leading-6 text-white" style="background: linear-gradient(to left, rgb(119, 119, 255), rgb(153, 0, 255)); margin-right: 30px; padding: 5px 15px; border-radius: 5px;">Sign Up <span aria-hidden="true">&rarr;</span></a>
+                <a href="/login" class="text-sm font-semibold leading-6 text-white" style="background: linear-gradient(to left, rgb(119, 119, 255), rgb(153, 0, 255)); margin-right: 0px; padding: 5px 15px; border-radius: 5px;">Log in <span aria-hidden="true">&rarr;</span></a>
+                @endauth
             </div>
             <div class="hidden lg:flex lg:gap-x-12">
                 <a href="/movies" class="text-sm font-semibold leading-6 text-gray-900">Movies</a>
@@ -36,6 +85,7 @@
             </div>
             <div class="hidden lg:flex lg:flex-1 lg:justify-end">
                 @auth
+
                 <a href="/home" style="margin-top:8px;"><span id="user-info" class="text-sm font-semibold leading-6 text-white bg-gradient-to-r from-purple-600 to-indigo-600 px-4 py-2 rounded-md mr-4" style="margin-top:17px; padding:10px 14px;">
                         {{ Auth::user()->name }}</a>
                 </span>
